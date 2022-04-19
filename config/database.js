8a90ab8d5af94f3dddefbfca937b5a98144432c7
Luis Martinez <mrluismartinezzz@gmail.com>
2022-04-18 19:09:13 -0700

    changing DB client to postgres

--- config/database.js
+++ config/database.js
@@ -1,11 +1,18 @@
-const path = require('path');
+const path = require("path");
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+module.exports = ({ env }) => {
+  return {
+    // connection obj = database configuration options passed to Knex.js
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client: "postgres",
+      connection: {
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi"),
+        user: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "strapi"),
+      },
+      useNullAsDefault: true,
     },
-    useNullAsDefault: true,
-  },
-});
+  };
+};
