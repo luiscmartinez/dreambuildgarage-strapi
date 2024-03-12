80b6efdb22aaa60e48066fb9aecab3c1e190d3c9
Luis Martinez <mrluismartinezzz@gmail.com>
2024-03-12 12:41:22 -0700

    adding connectionString to production db connection

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,10 +1,12 @@
 const parse = require("pg-connection-string").parse;
 const config = parse(process.env.DATABASE_URL);
+console.log("DATABASE_URL", process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
+      connectionString: env("DATABASE_URL"),
       host: config.host,
       port: config.port,
       database: config.database,
